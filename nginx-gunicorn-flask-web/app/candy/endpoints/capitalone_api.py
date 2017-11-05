4da24d12c1f78f2348bf49a6e9ce92380ea24674
lizhong <z.lizhong@gmail.com>
2017-11-05 01:12:44 -0500

    Add family profile endpoint

---
+++ nginx-gunicorn-flask-web/app/candy/endpoints/capitalone_api.py
@@ -0,0 +1,3 @@
+accounts_api = "https://3hkaob4gkc.execute-api.us-east-1.amazonaws.com/prod/au-hackathon/accounts"
+customers_api = "https://3hkaob4gkc.execute-api.us-east-1.amazonaws.com/prod/au-hackathon/customers"
+transactions_api = "https://3hkaob4gkc.execute-api.us-east-1.amazonaws.com/prod/au-hackathon/transactions"
